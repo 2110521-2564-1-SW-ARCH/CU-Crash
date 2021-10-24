e0ff4451027bf42d5a7435c2d6b7000bc2b24525
Kantapon <owliang1234@gmail.com>
2021-10-24 20:14:06 +0700

    Add Navbar logic

---
+++ frontend/src/components/common/CustomNavbar.js
@@ -0,0 +1,57 @@
+import { Container, Button, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
+
+export default function CustomNavbar() {
+    const username = "USERNAME";
+    const history = useHistory();
+
+    const onLogout = () => {
+        let isLogout = true;
+        console.log('call changePassword');
+        // try {
+        //   const res = await axios({});
+        //   console.log(res.data);
+        // } catch (e) {
+        //   console.log(e)
+        // }
+        if (isLogout) {
+            console.log('logout successfully')
+            history.replace("/");
+            history.go(0);
+        } else {
+            console.log('logout fail')
+        }
+    }
+
+    return (
+        <Navbar bg="light" expand="lg" sticky="top">
+            <Container>
+                <Navbar.Brand href="/home">CU-CRASH</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <NavDropdown title="Reviews" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/reviews/subject">
+                        Subject
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/reviews/instructor">
+                        Instructor
+                    </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="/supplementaries">Supplementaries</Nav.Link>
+                    <Nav.Link href="/settings">Settings</Nav.Link>
+                </Nav>
+                <Navbar.Text>
+                    Signed in as: {username}
+                </Navbar.Text>
+                <Button variant="primary" type="submit" className="mx-4" onClick={onLogout}>
+                    Log out
+                </Button>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
--- frontend/src/components/common/PageContainer.js
+++ frontend/src/components/common/PageContainer.js
@@ -1,48 +1,15 @@
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Navbar,
-  Nav,
-  NavDropdown,
-} from "react-bootstrap";
-
-const CustomNavBar = () => {
-  return (
-    <Navbar bg="light" expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand href="/home">CU-CRASH</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <NavDropdown title="Reviews" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/reviews/subject">
-                Subject
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/reviews/instructor">
-                Instructor
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="/supplementaries">Supplementaries</Nav.Link>
-            <Nav.Link href="/settings">Settings</Nav.Link>
-          </Nav>
-          <Button variant="primary" type="submit">
-            Log out
-          </Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
+import { Container, Row, Col } from "react-bootstrap";
+import { useLocation } from 'react-router-dom'
+import CustomNavbar from "./CustomNavbar";
 
 export default function PageContainer(props) {
+  const location = useLocation();
+  const showNavbar = location.pathname !== '/';
+
   return (
     <>
-      <CustomNavBar />
-      <Container style={{ color: "darkblue" }} className="mt-4" fluid="md">
+      {showNavbar && <CustomNavbar />}
+      <Container style={{ color: "darkblue" }} fluid="md" className="pt-16">
         <Row>
           <Col>{props.children}</Col>
         </Row>
